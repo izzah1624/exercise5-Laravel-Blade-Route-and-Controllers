af7cd5e1b10dc1cae9939ea822d2acef504ae7e2
izzah1624 <61498548+izzah1624@users.noreply.github.com>
2021-04-20 15:11:52 +0800

    Add files via upload

---
+++ about.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+@section('title', 'About Us')
+@section('content')
+ <h1>About Us</h1>
+
+ <p> E-book Sharing Application was established in 2020. </p>
+ <p>Students can use this website to read book online. They dont need to bring the heavy book anymore.</p>
+ <p> Hopefully this application can make it easier for students to learn and study especially during this online class. </p>
+ <p>This application would be the great ones!</p>
+
+@endsection 
