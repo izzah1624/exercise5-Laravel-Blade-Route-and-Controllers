c2259892db52bf6e5f0ddcd645dc05ca604931d8
izzah1624 <61498548+izzah1624@users.noreply.github.com>
2021-04-20 17:30:52 +0800

    Add files via upload

---
+++ nav.blade.php
@@ -0,0 +1,13 @@
+<!--nav-bar-->
+
+@section ('navbar')
+<div class = "navbar">
+   <div class ="navbar-inner">
+    <ul class="nav">
+    <li><a href="/about"> About</a> </li>
+ <li><a href="/list"> List</a></li>
+   
+    </ul>
+    </div>
+    </div>
+
